fb2828b94d37a406fc3fa0d9af7dfb866c45737b
Harry <yury_regis@hotmail.com>
2020-11-15 15:39:58 -0300

    Upload cmd ENVIARPV

---
+++ comandos/enviarpv.js
@@ -0,0 +1,41 @@
+const { verificaRole } = require('../funcoes/roles'),
+      getID            = require('../funcoes/ids.json');
+
+
+exports.help = {
+    name: "enviarpv"
+}
+
+exports.run = async (client, message, args) => {
+    await message.delete();
+    
+    if(verificaRole(message.member, getID.cargo.ADMIN) || verificaRole(message.member, getID.cargo.STAFF)) {
+        let mensaoUsuario = /@/;
+        let canal = await message.channel;
+
+        if(!isNaN(args[0])) {
+            let membro = await canal.guild.members.cache.get(args[0]);
+
+            if(!membro) {
+                return canal.send("Membro não encontrado.");
+            }
+            return membro.send( `${membro.user.username}: ${args.slice(1).join(" ")}` )
+                .then()
+                .catch(err => { canal.send("Terminal `!enviarpv`" + `\`\`\`${err}\`\`\``) });          
+        } 
+        if(mensaoUsuario.test(args[0])) {
+            let membro = message.mentions.members.first();
+
+            if(!membro) {
+                return canal.send(`Membro não encontrado.`);
+            }
+            return membro.send( `${membro.user.username}: ${args.slice(1).join(" ")}` )
+                .then()
+                .catch(err => { canal.send("Terminal `!enviarpv`" + `\`\`\`${err}\`\`\``) }); 
+        } else {
+            return message.reply("Erro de sintaxe. Use `!enviarpv @usuario` ou `!enviarpv idUsuario`");
+        }
+    } else {
+        return message.reply("Apenas @Staff ou @Admin podem usar este comando.");
+    }
+}
