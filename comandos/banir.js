2a922eb1ca5979409a364e8821b87e2aead733e6
Harry <yury_regis@hotmail.com>
2020-11-15 15:15:44 -0300

    Upload cmd BANIR

---
+++ comandos/banir.js
@@ -0,0 +1,70 @@
+
+
+const { verificaRole } = require('../funcoes/roles'),
+      { userToMember } = require('../funcoes/members'),
+      { embedSimples } = require('../funcoes/messages'),
+      getID            = require('../funcoes/ids.json');
+
+
+exports.help = {
+    name: "banir"
+}
+
+
+exports.run = async (client, message, args) => {
+    
+    if(!verificaRole(message.member, "Admin") || !verificaRole(message.member, "Staff"))
+        return message.reply(`você não possui permissão.`);
+
+    const salaLogs   = await client.channels.cache.get(getID.sala.LOGS),
+          salaAviso  = await client.channels.cache.get(getID.sala.AVISOS),
+          membroAlvo = await message.mentions.users.first(),
+          motivoBan  = args.slice(1).join(" ");
+    
+    if(membroAlvo === undefined)
+        return message.reply(`não detectei nenhuma menção de usuário no comando.`);
+    
+    let msgMembro  = `${message.author} baniu você pelo seguinte motivo:\n\n${motivoBan}\n\n` +
+                           `Você não poderá interagir no servidor **ThatSkyGameBrasil**`,
+        membroUser = await userToMember(membroAlvo, message),
+        nomeMembro = membroAlvo.tag;
+    
+    // Mensagem privada para o membro banido
+    await membroAlvo.send(msgMembro)
+        .then( () => salaLogs.send(`Membro ${membroAlvo} banido.\n\`\`\`MOTIVO:\n${motivoBan}\`\`\``))
+        .catch(erro => {
+            console.log(`Ban error => ${erro}`);
+            salaLogs.send(`Ban Error (Terminal):\n\`\`\`Não posso enviar mensagem privada\n\n${erro}\`\`\``);
+        });
+    
+    // Banindo membro e enviando aviso após banimento
+    await membroUser.ban({reason: motivoBan + `Você não poderá interagir no servidor TSGB`})
+        .then( async () => {
+            let cor    = '#CF3F47',
+                titulo = '**BANIDO**',
+                thumb  = 'https://i.ibb.co/FD93h6p/KRILL.png',
+                imagem = 'https://media1.tenor.com/images/7129d4fbd2bd63ab987a768951ff44cb/tenor.gif',
+                descricao = `OPA! Parece que ${nomeMembro} foi pego por um Krill !!!`,
+                embed  = await embedSimples(cor, titulo, thumb, descricao, imagem);
+            
+            salaAviso.send(embed)
+                .then( async msg => {
+                    // Reagir a mensagem com emotes do Krill
+                    let cabeca = await client.emojis.cache.get('698231847330644068'), 
+                        corpo  = await client.emojis.cache.get('698231902993121370'), 
+                        calda  = await client.emojis.cache.get('698231942205931570');
+
+                    await msg.react(cabeca);
+                    await msg.react(corpo);
+                    await msg.react(calda);
+                })
+                .catch(err => salaLogs.send(`Terminal Ban\n\`\`\`${err}\`\`\``))
+        })
+        .catch(err => {
+            console.log(`Ban error => ${err}`);
+            salaLogs.send(`Terminal Ban\n\`\`\`${err}\`\`\``);
+        });
+
+    message.delete();
+    return;
+}
