8aaf5c5c1ac47814e053901bf9e3a166a5dc138b
Harry <yury_regis@hotmail.com>
2020-11-18 19:59:10 -0300

    Comando atualizado para v12

--- comandos/del.js
+++ comandos/del.js
@@ -8,18 +8,21 @@ exports.run = async (client, message, args) => {
     const salaAtual = message.channel;
     const salaLogs  = client.channels.cache.get(getID.sala.LOGS2);
 
-    if(verificaPerm(message.member)) {
+    if( await verificaPerm(message.member)) {
         try {
             let num = 2;
             if(!isNaN(args[0]))
                 num = parseInt(args[0]) + 1;
 
-            let captura = await salaAtual.messages.fetch({limit: num});
-            await salaAtual.bulkDelete(captura);
-                             
-            const m = await salaAtual.send(`${num} mensagens apagadas...`);
+            //let captura = await salaAtual.messages.fetch({limit: num});
             
-            m.delete(30000)
+            await salaAtual.bulkDelete(num)
+                .then(async messages => {
+                    console.log(`${(messages.size - 1)} deletadas por ${message.author.username}`);
+                    salaLogs.send(`${(messages.size - 1)} deletadas por ${message.author.username}`);
+                    const m = await salaAtual.send(`${num} mensagens apagadas...`);
+                    m.delete(3000);
+                })
                 .catch(err => { salaLogs.send(`Terminal \`!del\` \`\`\`${err}\`\`\``) });
         } 
         catch(err) {
