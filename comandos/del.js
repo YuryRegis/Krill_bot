f17fd76e6ffa87fdf64a4d2bd2de04aef3f15035
Harry <yury_regis@hotmail.com>
2020-11-18 20:48:30 -0300

    Codigo otimizado v12

--- comandos/del.js
+++ comandos/del.js
@@ -5,11 +5,11 @@ const getID            = require('../funcoes/ids.json'),
 //comando para apagar multiplas mensagens
 exports.run = async (client, message, args) => {
     
-    const salaAtual = message.channel;
+    const salaAtual = await message.channel;
     const salaLogs  = await client.channels.cache.get(getID.sala.LOGS2),
           permissao = await verificaPerm(message.member);
 
-    if(!permissao)
+    if(permissao)
         return message.reply("te falta **poderes** para usar este comando!");
 
     else {
@@ -18,16 +18,19 @@ exports.run = async (client, message, args) => {
             if(!isNaN(args[0]))
                 num = parseInt(args[0]) + 1;
 
-            //let captura = await salaAtual.messages.fetch({limit: num});
+            let captura = await salaAtual.messages.fetch({limit: num});
             
-            await salaAtual.bulkDelete(num)
+            salaAtual.bulkDelete(parseInt(captura.size), true)
                 .then(async messages => {
                     console.log(`${(messages.size - 1)} deletadas por ${message.author.username}`);
                     salaLogs.send(`${(messages.size - 1)} deletadas por ${message.author.username}`);
                     const m = await salaAtual.send(`${num} mensagens apagadas...`);
-                    m.delete(3000);
+                    await m.delete({ timeout: 3000 });
                 })
-                .catch(err => { salaLogs.send(`Terminal \`!del\` \`\`\`${err}\`\`\``) });
+                .catch(err => { 
+                    salaLogs.send(`Terminal \`!del\` \`\`\`${err}\`\`\``);
+                    console.log("BulkDelete error -> ",err);
+                });
         } 
         catch(err) {
             console.log(err);
