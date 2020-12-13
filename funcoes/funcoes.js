9b6a189ff422c3e58b75f68ed3b471a4432ecf05
Harry <yury_regis@ufrj.br>
2020-12-13 02:36:58 -0300

    Add função verificaPalavrão + regex otimizado

--- funcoes/funcoes.js
+++ funcoes/funcoes.js
@@ -1,3 +1,5 @@
+const regex = /p[oòóôõö]+(rr|h)[aàáâãä]|p[oòóôõö](rr|h)[\S]+nh[\S]|(c|k)[\S]+r[\S]+lh[\S]+|(c|k)[uùúüû]+(\s|\n|$|!|\.|\?)|(c|k)[\duùúüû]+[sz][\S]+|b[\duùúüûoòóõôö]+(c|s)[\S]+t[\S]+|p[\diìíïî]+(c|k|ck)+[aàáäâ]|p[\diìíïî]+r[\doóòöô]+[ck][\S]+|(p[\duùúüû]+t[\daàáäâãoóòôõôeêéèë]+[\S]+)|[ck][\daàáâãä][cs][\S]+t[\S]|[ck][aàáâãä]7|p[\S]+r[\S]+r[\S]+[ck][\s\S]{1}|f[\doòóôõöuùúüû]+d[\S]+r|(x|ch)[\S]+(x|ch)[\S]+t[\S]+|(x|ch)[\doòóôõö]+t[\daáàãâä]+|b[\doòóõôö]+(qu|k)[\S]+t[\S]+|p[\deéèêë]+n+[\diíìîï]+s|[\daàáâãä]+n+[\daàáâãä]+(u|l)+(\s|\n|\.|\?|!)|[\daàáâãä]+n+[uùúûü]+s|[\doòóôõö]+t+[\daàáâãä]+r+[\S]+|b[\S]b[\S](c|K)(\S|$|\s)|pr[\S]+(x|ch)[\S]+(c|k)(\S|$|\s|\n)|p[\S]+s+y|(x|ch)[\S]+b+[\S]+(c|k)|(x|ch)[\daàáâãä]+n+[\daàáâãä]+|b[\doòóôõö]+g+[\daàáâãä]+|m[\deèéêë]+r+d+[\S]+|f[\doóòõôöuúùûü]+d+[\S]+|f[\S]+ck|d[\S]+ck/gi
+
 module.exports = {
 
     // retorna membro mencionado em uma mensagem ou por ID
@@ -39,7 +41,6 @@ module.exports = {
 
     // verifica presence update (status de presença)
     prsUPD: async function(data, client){
-        let regex   = /b[u,ù,ú,o,ô,0-9][c,s][e,ê,0-9]t[a,ã,0-9]|b[u,ù,ú,o,ô,0-9]ss[e,ê,0-9]t[a,ã,0-9]|p[a,à,á,0-9][u,ù,ú,l,0-9]|[c,k][u,ú,ù]|[c,k][u,ú,ù][z,s][a,à,á,ã,0-9]|x[u,ò,ó,0-9]x[u,ò,ó,0-9]t[a,à,á,ã,0-9]|m[a,à,á]m[a,à,á]|p[e,ê,é,è,0-9]n[i,ì,í,0-9][s,z]|p[i,ì,í,0-9][n,m]t[o,a,ã,0-9]|[c,k][y,i][u,ù,ú,0-9]|x[u,ú,ù,0-9]p[a,à,á,ã,0-9]|ch[u,ú,ù,0-9]p[a,á,à,ã,0-9]|p[u,ù,ú,0-9][n,m,0-9]h[e,ê,0-9]t[a,ã,e,ê,0-9]|ch[o,ó,ò,0-9]ch[o,ó,ò,0-9]t[a,ã,à,á0-9]|ch[o,ó,ò,0-9]t[a,ã,à,á0-9]|[0-9]cm|x[o,ó,ò,0-9]t[a,ã,à,á0-9]|r[o,ò,ó,ô,0-9]l[a,à,á,â,0-9]|p[i,í,ì,0-9][c,k][a,ã,0-9]|π[c,k]|π[c,k][a,ã,0-9]|[c,k][o,ó,ò,ô,0-9]m[e,ê,è,é,0-9]d[o,ó,ò,ô,0-9]|m[a,ã,0-9][e,0-9]|p[a,à,á,0-9][i,ì,í,0-9]|t[e,ê,0-9]t[a,à,á,0-9]|g[o,u,ô,0-9]z[a,e,ê,à,á,0-9]|l[a,â,ã,à,á,0-9]mb[o,ô,u,e,ê,i,í,ì,0-9]|b[o,u,ò,ó,ô,0-9][q,k,c][u,e,è,é,0-9][t,c]|r[a,à,á,0-9]b[a,ã,0-9]|s[a,à,á,0-9]p[e,è,é,ê,0-9][c,k][a,à,á,0-9]|l[e,ê,è,é,0-9][i,ì,í,0-9]t[e,ê,i,ã,0-9]|[e,i,0-9]ng[u,ô,ò,ó,0-9]l[a,i,o,ô,0-9]|[e,i,0-9]str[u,o,ô,0-9]p[a,à,á,0-9]d[a,o,ô,u,0-9]|[e,i,0-9]st[u,o,ô,0-9]p[a,0-9]d[u,o,a,ô,0-9]|b[u,o,0-9]nd[a,ã,0-9]|f[o,ò,ó,0-9]d[a,i,ì,í,e,ã,0-9]|bct[a,u,o,i,ã,0-9]|bct|v[a,à,á,0-9][g,j][i,e,ì,í,0-9][n,m][a,à,á,0-9]|p[i,ì,í,0-9]r[o,ó,ò,ô,0-9][c,k][a,à,á,u,ú,o,ã,0-9]|p[o,ó,ò,ô,õ,0-9]rr[a,à,á,ã,0-9]|p[o,ô,õ,0-9][r,h][a,à,á,ã,0-9]|[e,ê,i,0-9]sp[o,ô,õ,0-9][r,h][o,ô,a,e,ê,ã,0-9]|[e,i,0-9]sp[o,ô,õ,0-9]rr[o,ô,a,e,ê,ã,0-9]|f[u,ú,o,0-9][c,k][c,k]|[e,i,0-9][n,m][r,h][a,á,à,0-9]b[e,ê,o,ô,a,à,á,0-9]/
 
         if(data.d.game !== null) {            
             let state = data.d.game.state;
@@ -60,7 +61,6 @@ module.exports = {
 
     // status update (alteração de status do membro)
     mbrUPD: async function(data){
-        let regex   = /b[u,ù,ú,o,ô,0-9][c,s][e,ê,0-9]t[a,ã,0-9]|b[u,ù,ú,o,ô,0-9]ss[e,ê,0-9]t[a,ã,0-9]|p[a,à,á,0-9][u,ù,ú,l,0-9]|[c,k][u,ú,ù]|[c,k][u,ú,ù][z,s][a,à,á,ã,0-9]|x[u,ò,ó,0-9]x[u,ò,ó,0-9]t[a,à,á,ã,0-9]|m[a,à,á]m[a,à,á]|p[e,ê,é,è,0-9]n[i,ì,í,0-9][s,z]|p[i,ì,í,0-9][n,m]t[o,a,ã,0-9]|[c,k][y,i][u,ù,ú,0-9]|x[u,ú,ù,0-9]p[a,à,á,ã,0-9]|ch[u,ú,ù,0-9]p[a,á,à,ã,0-9]|p[u,ù,ú,0-9][n,m,0-9]h[e,ê,0-9]t[a,ã,e,ê,0-9]|ch[o,ó,ò,0-9]ch[o,ó,ò,0-9]t[a,ã,à,á0-9]|ch[o,ó,ò,0-9]t[a,ã,à,á0-9]|[0-9]cm|x[o,ó,ò,0-9]t[a,ã,à,á0-9]|r[o,ò,ó,ô,0-9]l[a,à,á,â,0-9]|p[i,í,ì,0-9][c,k][a,ã,0-9]|π[c,k]|π[c,k][a,ã,0-9]|[c,k][o,ó,ò,ô,0-9]m[e,ê,è,é,0-9]d[o,ó,ò,ô,0-9]|m[a,ã,0-9][e,0-9]|p[a,à,á,0-9][i,ì,í,0-9]|t[e,ê,0-9]t[a,à,á,0-9]|g[o,u,ô,0-9]z[a,e,ê,à,á,0-9]|l[a,â,ã,à,á,0-9]mb[o,ô,u,e,ê,i,í,ì,0-9]|b[o,u,ò,ó,ô,0-9][q,k,c][u,e,è,é,0-9][t,c]|r[a,à,á,0-9]b[a,ã,0-9]|s[a,à,á,0-9]p[e,è,é,ê,0-9][c,k][a,à,á,0-9]|l[e,ê,è,é,0-9][i,ì,í,0-9]t[e,ê,i,ã,0-9]|[e,i,0-9]ng[u,ô,ò,ó,0-9]l[a,i,o,ô,0-9]|[e,i,0-9]str[u,o,ô,0-9]p[a,à,á,0-9]d[a,o,ô,u,0-9]|[e,i,0-9]st[u,o,ô,0-9]p[a,0-9]d[u,o,a,ô,0-9]|b[u,o,0-9]nd[a,ã,0-9]|f[o,ò,ó,0-9]d[a,i,ì,í,e,ã,0-9]|bct[a,u,o,i,ã,0-9]|bct|v[a,à,á,0-9][g,j][i,e,ì,í,0-9][n,m][a,à,á,0-9]|p[i,ì,í,0-9]r[o,ó,ò,ô,0-9][c,k][a,à,á,u,ú,o,ã,0-9]|p[o,ó,ò,ô,õ,0-9]rr[a,à,á,ã,0-9]|p[o,ô,õ,0-9][r,h][a,à,á,ã,0-9]|[e,ê,i,0-9]sp[o,ô,õ,0-9][r,h][o,ô,a,e,ê,ã,0-9]|[e,i,0-9]sp[o,ô,õ,0-9]rr[o,ô,a,e,ê,ã,0-9]|f[u,ú,o,0-9][c,k][c,k]|[e,i,0-9][n,m][r,h][a,á,à,0-9]b[e,ê,o,ô,a,à,á,0-9]/
 
         let nome = data.d.user.username,
             id   = data.d.user.id,
@@ -82,6 +82,13 @@ module.exports = {
         return null;
     },
 
+    // retorna lista de palavões ou falso para caso de não encontrar palavrões
+    verificaPalavrao: async function(message) {
+        listaPalavrao = message.match(regex); 
+        if(listaPalavrao === null)
+            return false;
+        return listaPalavrao;
+    },
 
     // retorna data em formato PT-BR
     formatDate: function(date) {
